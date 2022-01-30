aebc471da73a0633088b29ca4d8f11dd2863a98c
Puru-007 <purveshpatil660@gmail.com>
2022-01-30 10:48:42 +0530

    Boilerplate code added from Spring init for Load Balancer Server

---
+++ Simple-Linear-Load-Balancer/src/main/java/com/simpleLB/SimpleLinearLoadBalancer/SimpleLinearLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.simpleLB.SimpleLinearLoadBalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLinearLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLinearLoadBalancerApplication.class, args);
+	}
+
+}
---
+++ Simple-Linear-Load-Balancer/src/test/java/com/simpleLB/SimpleLinearLoadBalancer/SimpleLinearLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleLB.SimpleLinearLoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLinearLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
